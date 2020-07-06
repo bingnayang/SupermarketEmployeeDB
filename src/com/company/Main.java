cfab036d8f60c78440692827d5eb5ff832e257e1
BingNa Yang <bingnayang@gmail.com>
2020-07-06 15:58:43 -0400

    add query to search employee by department name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,13 +15,32 @@ public class Main {
             return;
         }
 
+        // Print all employees information in database
+        System.out.println("----> Print All Employee Information <----");
         List<EmployeeInfo> employeeInfos = datasource.queryEmployeeInfo();
-        System.out.println("___Print All Employee___");
         if(employeeInfos.isEmpty()){
             System.out.println("Couldn't find");
         }
         for(EmployeeInfo employee: employeeInfos){
-            System.out.println(employee.getEmployee_Id()+" "+employee.getFirst_Name()+" "+employee.getLast_Name());
+            System.out.println();
+            System.out.println("Employee ID: "+employee.getEmployee_Id()+
+                    "\nName: "+employee.getFirst_Name()+" "+employee.getLast_Name()+
+                    "\nSalary: $"+employee.getSalary()+
+                    "\nTitle: "+employee.getTitle_Name()+
+                    "\nDepartment: "+employee.getDepartment_Name()+
+                    "Start Date: "+employee.getStart_Date()+
+                    "\nEnd Date: "+employee.getEnd_Date());
+        }
+
+
+        // Print employees by department
+        System.out.println("----> Print Employees By Department <----");
+        List<String> employeesByDepartment = datasource.queryEmployeeByDepartment("Front End");
+        if(employeesByDepartment.isEmpty()){
+            System.out.println("Couldn't find");
+        }
+        for(String list: employeesByDepartment){
+            System.out.println(list);
         }
 
         // Close connection
--- src/com/company/model/Datasource.java
+++ src/com/company/model/Datasource.java
@@ -25,7 +25,7 @@ public class Datasource {
     public static final String COLUMN_DEPARTMENT_ID = "department_Id";
     public static final String COLUMN_DEPARTMENT_NAME = "department_Name";
     // Title Table
-    public static final String TABLE_Title = "title";
+    public static final String TABLE_TITLE = "title";
     public static final String COLUMN_TITLE_ID = "title_Id";
     public static final String COLUMN_TITLE_NAME = "title_Name";
     public static final String COLUMN_TITLE_DEPARTMENT_ID = "department_Id";
@@ -33,7 +33,7 @@ public class Datasource {
     public static final String TABLE_Employment_Status = "employmentStatus";
     public static final String COLUMN_EMPLOYMENT_STATUS_ID = "employment_Status_Id";
     public static final String COLUMN_EMPLOYMENT_STATUS = "employment_Status";
-    // EmployeeInfo Table
+    // EmployeeInfo View Table
     public static final String TABLE_EMPLOYEEINFO = "employeeInfo";
     public static final String COLUMN_EMPLOYEEINFO_EMPLOYEE_ID = "employee_Id";
     public static final String COLUMN_EMPLOYEEINFO_FIRST_NAME = "first_Name";
@@ -45,16 +45,32 @@ public class Datasource {
     public static final String COLUMN_EMPLOYEEINFO_END_DATE = "endDate";
 
     /**
-     * SQL:
+     * SQL: SELECT, DELETE, UPDATE, INSERT
      */
-    //    SELECT * FROM employeeInfo
-    private static final String QUERY_EMPLOYEES = "SELECT * FROM "+TABLE_EMPLOYEEINFO;
+    //  SELECT *
+    //  FROM employeeInfo
+    private static final String QUERY_EMPLOYEES =
+            "SELECT * FROM "+TABLE_EMPLOYEEINFO;
+
+    //  SELECT employee.first_name, employee.last_name, title.title_Name
+    //  FROM employee
+    //  INNER JOIN title, department
+    //  WHERE employee.title_Id = title.title_Id
+    //  AND department.department_Id = title.department_Id
+    //  AND department.department_Name ="Front End"
+    private static final String QUERY_EMPLOYEES_BY_DEPARTMENT =
+            "SELECT "+TABLE_EMPLOYEE+"."+COLUMN_EMPLOYEE_FIRST_NAME+","+TABLE_EMPLOYEE+"."+COLUMN_EMPLOYEE_LAST_NAME+","+TABLE_TITLE+"."+COLUMN_TITLE_NAME+
+            " FROM "+TABLE_EMPLOYEE+
+            " INNER JOIN "+TABLE_TITLE+","+TABLE_DEPARTMENT+
+            " WHERE "+TABLE_EMPLOYEE+"."+COLUMN_EMPLOYEE_TITLE_ID+"="+TABLE_TITLE+"."+COLUMN_TITLE_ID+
+            " AND "+TABLE_DEPARTMENT+"."+COLUMN_DEPARTMENT_ID+"="+TABLE_TITLE+"."+COLUMN_TITLE_DEPARTMENT_ID+
+            " AND "+TABLE_DEPARTMENT+"."+COLUMN_DEPARTMENT_NAME+"=?";
 
 
     private Connection connection;
 
-    // Use Prepared Statement
     private PreparedStatement queryEmployees;
+    private PreparedStatement queryEmployeesByDepartment;
 
 
     // Open Connection
@@ -63,6 +79,7 @@ public class Datasource {
             connection = DriverManager.getConnection(CONNECTION_STRING);
 
             queryEmployees = connection.prepareStatement(QUERY_EMPLOYEES);
+            queryEmployeesByDepartment = connection.prepareStatement(QUERY_EMPLOYEES_BY_DEPARTMENT);
 
             return true;
         }catch (SQLException e){
@@ -76,6 +93,9 @@ public class Datasource {
         try{
             if(queryEmployees != null){
                 queryEmployees.close();
+            }
+            if(queryEmployeesByDepartment != null){
+                queryEmployeesByDepartment.close();
             }
 
             if (connection != null) {
@@ -95,6 +115,11 @@ public class Datasource {
                 employeeInfo.setEmployee_Id(resultSet.getInt(1));
                 employeeInfo.setFirst_Name(resultSet.getString(2));
                 employeeInfo.setLast_Name(resultSet.getString(3));
+                employeeInfo.setSalary(resultSet.getDouble(4));
+                employeeInfo.setTitle_Name(resultSet.getString(5));
+                employeeInfo.setDepartment_Name(resultSet.getString(6));
+                employeeInfo.setStart_Date(resultSet.getString(7));
+                employeeInfo.setEnd_Date(resultSet.getString(8));
                 employeesInfoList.add(employeeInfo);
             }
 
@@ -104,5 +129,20 @@ public class Datasource {
             return null;
         }
     }
+    public List<String> queryEmployeeByDepartment(String deptName){
+        try{
+            queryEmployeesByDepartment.setString(1,deptName);
+            ResultSet resultSet = queryEmployeesByDepartment.executeQuery();
 
+            List<String> employeesList = new ArrayList<>();
+            while (resultSet.next()){
+                employeesList.add("Name: "+resultSet.getString(1)+" "+resultSet.getString(2)+
+                                    "\nTitle: "+resultSet.getString(3)+"\n");
+            }
+            return employeesList;
+        }catch (SQLException e){
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
+        }
+    }
 }
